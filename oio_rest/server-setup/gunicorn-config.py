4e002268cd9a583063bc42dc92c5ff2a7d48a703
Dan Villiom Podlaski Christiansen <dan@magenta.dk>
2016-11-15 15:30:17 +0100

    Docker: use 'setup.py develop' on ubuntu
    The previous Dockerfile was simpler, but duplicated the list of
    requirements to adapt to Docker's python image. This file ensures that
    the generated container matches what we usually install fairly well.
    
    I've made an effort to make to clean it up as much as possible after
    setup.

---
+++ oio_rest/server-setup/gunicorn-config.py
@@ -0,0 +1,19 @@
+import multiprocessing
+
+bind = ':8000'
+# workers = multiprocessing.cpu_count() * 2 + 1
+# worker_class = 'gevent'
+# preload_app = True
+
+user = 'mox'
+group = 'mox'
+
+accesslog = '/var/log/oio_rest/access.log'
+errorlog = '/var/log/oio_rest/error.log'
+
+proc_name = 'oio_rest'
+#pidfile = '/var/run/oio_rest.pid'
+
+raw_env = [
+    'NO_VENV=1',
+]
