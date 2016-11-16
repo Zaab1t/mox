493fdb7547416f2110d22f4f0396aea77646a03a
Dan Villiom Podlaski Christiansen <dan@magenta.dk>
2016-11-16 15:37:26 +0100

    Docker: minor tweaks
    - include full sources
    - add a comment concerning possible futility of decreasing footprint
    - simplify installing build dependancies
    - re-enable gevent worker for gunicorn

--- oio_rest/server-setup/gunicorn-config.py
+++ oio_rest/server-setup/gunicorn-config.py
@@ -1,9 +1,9 @@
 import multiprocessing
 
 bind = ':8000'
-# workers = multiprocessing.cpu_count() * 2 + 1
-# worker_class = 'gevent'
-# preload_app = True
+workers = multiprocessing.cpu_count() * 2 + 1
+worker_class = 'gevent'
+preload_app = True
 
 user = 'mox'
 group = 'mox'
@@ -12,7 +12,7 @@ accesslog = '/var/log/oio_rest/access.log'
 errorlog = '/var/log/oio_rest/error.log'
 
 proc_name = 'oio_rest'
-#pidfile = '/var/run/oio_rest.pid'
+pidfile = '/var/run/oio_rest.pid'
 
 raw_env = [
     'NO_VENV=1',
