c6bdb8bab71cd4a18b3307e17276414a7eaffaa3
Lars Peter Thomsen <lapeth@gmail.com>
2016-12-28 12:47:32 +0100

    Run moxwiki as a package

--- agents/MoxWiki/moxwiki.sh
+++ agents/MoxWiki/moxwiki.sh
@@ -6,7 +6,7 @@ AS_USER="mox"
 
 cd $DIR > /dev/null
 if [[ `whoami` != "$AS_USER" ]]; then
-    sudo su $AS_USER -c "source python-env/bin/activate && python moxwiki/app.py && deactivate"
+    sudo su $AS_USER -c "source python-env/bin/activate && python -m moxwiki.app && deactivate"
 else
     source python-env/bin/activate && python -m moxwiki.app && deactivate
 fi
