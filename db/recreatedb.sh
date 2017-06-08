933d48478e0d069e29be3fae70c580d89d6cc7ab
Dan Villiom Podlaski Christiansen <dan@magenta.dk>
2017-06-08 14:53:54 +0200

    Add confirmation prompt to recreatedb.sh

--- db/recreatedb.sh
+++ db/recreatedb.sh
@@ -1,11 +1,24 @@
 #!/bin/bash -e
-set -x
 set -b
 
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
 cd $DIR
 source ./config.sh
+
+read -r -p \
+     "You are about to destroy ALL DATA in your current database. Proceed? [y/N] " \
+     input
+
+case $input in
+    [yY][eE][sS]|[yY])
+		# do nothing
+		;;
+
+    *)
+	exit 1
+	;;
+esac
 
 export PGPASSWORD="$MOX_DB_PASSWORD"
 # TODO: Support remote Postgres DB server
