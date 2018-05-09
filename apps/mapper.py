1d2bd943f5207b00d0de429c3a2de4dff2fcfd01
Robert Jensen <jensen.robert@gmail.com>
2018-05-09 11:48:06 +0200

    Code is now able to read and list mappings

--- apps/mapper.py
+++ apps/mapper.py
@@ -24,14 +24,30 @@ class KlasseMapper(object):
         klasse_list = response.json()['results'][0]
         return klasse_list
 
-    def create_assymetric_mapping(self, uuid_from, uuid_to):
+    def _create_assymetric_mapping(self, uuid_from, uuid_to):
+        """ Map uui_from to uuid_to
+        :param uuid_from: The Klasse to recieve the mapping
+        :param uuid_to: The klasse that uuid_from will now map to
+        :return: Returns True if success
+        """
         virkning = {'from': '1801-01-01 12:12:12', 'to': 'infinity'}
         template = {'relationer': {'mapninger':
                                    [{'uuid': '', # Will be filled in
                                      'objekttype': 'Klasse',
                                      'virkning': virkning}]}}
+        error_msg = ''
+        success = False
+        klasse = self.read_klasse(uuid_from)
+        mappings = klasse['relationer']['mapninger']
+        print(mappings)
 
-        pass
+    def create_mapping(self, uuid_1, uuid_2, symmetric=True):
+        """Maps two uuids
+        :param uuid_1: One of the Klasser to be mapped
+        :param uuid_2: The other Klasse in the mapping
+        :symmetric: If false, the mapping will be one-way - be careful!
+        :return: Returns True if success, otherwise False and error message
+        """
     
     def read_klasse(self, uuid):
         """
@@ -42,6 +58,22 @@ class KlasseMapper(object):
         response = requests.get(self.hostname + self.url + uuid)
         klasse = response.json()[uuid][0]['registreringer'][0]
         return klasse
+
+    def read_mappings(self, klasse):
+        """ Read all mappings from a klasse
+        :param klasse: Klasse object as return by read_klasse, or uuid
+        :return: Information about the Klasse ie. Brugervendt nøgle
+        """
+        actual_mappings = []
+        if isinstance(klasse, str):
+            klasse = self.read_klasse(klasse)
+        try:
+            mapping_list = klasse['relationer']['mapninger']
+            for mapping in mapping_list:
+                actual_mappings.append(mapping['uuid'])
+        except KeyError:
+            pass
+        return actual_mappings
 
     def basic_klasse_info(self, klasse):
         """ Read human-readable data from Klass
@@ -59,10 +91,16 @@ def main():
     lora_hostname = 'http://mox-steffen:8080'
     mapper = KlasseMapper(lora_hostname)
 
+    uuid_1 = 'd8043094-6f38-4349-9fbb-dc7c28668fa0'
+    uuid_2 = '08857eb8-a2c4-4337-836f-19332f991362'
+    #print(mapper._create_assymetric_mapping(uuid_1, uuid_2))
     klasse_list = mapper.read_klasse_list()
     for uuid in klasse_list:
-        print(mapper.basic_klasse_info(uuid))
-
+        mapping = mapper.read_mappings(uuid)
+        if mapping:
+            print(uuid)
+            print(mapper.basic_klasse_info(uuid))
+            print('-----------')
 
 if __name__ == '__main__':
     main()
