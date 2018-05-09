0191e1d9d162cb6439bc0a1d8e8367f9ed0e314a
Robert Jensen <jensen.robert@gmail.com>
2018-05-09 13:18:18 +0200

    Mapper now works for actual mapping of two Klasser

--- apps/mapper.py
+++ apps/mapper.py
@@ -3,6 +3,7 @@
 between OIO classes (Klasse) """
 import requests
 
+
 class KlasseMapper(object):
     """ Small prototype of an application that will allow for mapping
     between OIO classes (Klasse) """
@@ -24,31 +25,6 @@ class KlasseMapper(object):
         klasse_list = response.json()['results'][0]
         return klasse_list
 
-    def _create_assymetric_mapping(self, uuid_from, uuid_to):
-        """ Map uui_from to uuid_to
-        :param uuid_from: The Klasse to recieve the mapping
-        :param uuid_to: The klasse that uuid_from will now map to
-        :return: Returns True if success
-        """
-        virkning = {'from': '1801-01-01 12:12:12', 'to': 'infinity'}
-        template = {'relationer': {'mapninger':
-                                   [{'uuid': '', # Will be filled in
-                                     'objekttype': 'Klasse',
-                                     'virkning': virkning}]}}
-        error_msg = ''
-        success = False
-        klasse = self.read_klasse(uuid_from)
-        mappings = klasse['relationer']['mapninger']
-        print(mappings)
-
-    def create_mapping(self, uuid_1, uuid_2, symmetric=True):
-        """Maps two uuids
-        :param uuid_1: One of the Klasser to be mapped
-        :param uuid_2: The other Klasse in the mapping
-        :symmetric: If false, the mapping will be one-way - be careful!
-        :return: Returns True if success, otherwise False and error message
-        """
-    
     def read_klasse(self, uuid):
         """
         Read a Klasse from LoRa
@@ -69,10 +45,13 @@ class KlasseMapper(object):
             klasse = self.read_klasse(klasse)
         try:
             mapping_list = klasse['relationer']['mapninger']
-            for mapping in mapping_list:
-                actual_mappings.append(mapping['uuid'])
         except KeyError:
-            pass
+            mapping_list = []
+        for mapping in mapping_list:
+            try:
+                actual_mappings.append(mapping['uuid'])
+            except KeyError:
+                pass
         return actual_mappings
 
     def basic_klasse_info(self, klasse):
@@ -86,21 +65,64 @@ class KlasseMapper(object):
         info = egenskaber['brugervendtnoegle']
         return info
 
+    def _create_assymetric_mapping(self, uuid_from, uuid_to):
+        """ Map uui_from to uuid_to
+        :param uuid_from: The Klasse to recieve the mapping
+        :param uuid_to: The klasse that uuid_from will now map to
+        :return: Returns True if success
+        """
+        virkning = {'from': '1801-01-01 12:12:12', 'to': 'infinity'}
+        template = {'relationer': {'mapninger':
+                                   [{'uuid': uuid_to,
+                                     'objekttype': 'Klasse',
+                                     'virkning': virkning}]}}
+
+        klasse = self.read_klasse(uuid_from)
+        print(self.basic_klasse_info(klasse))
+        for mapping in klasse['relationer']['mapninger']:
+            template['relationer']['mapninger'].append(mapping)
+        response = requests.patch(self.hostname + self.url + uuid_from,
+                                  json=template)
+        return response.json()['uuid'] == uuid_from
+
+    def create_mapping(self, uuid_1, uuid_2, symmetric=True):
+        """Maps two uuids
+        :param uuid_1: One of the Klasser to be mapped
+        :param uuid_2: The other Klasse in the mapping
+        :symmetric: If false, the mapping will be one-way - be careful!
+        :return: Returns True if success, otherwise False and error message
+        """
+        error_msg = ''
+        success = False
+        map_1 = self.read_mappings(uuid_1)
+        map_2 = self.read_mappings(uuid_2)
+        if uuid_2 in map_1 or uuid_1 in map_2:
+            error_msg = 'Mapping already exists'
+        else:
+            success = self._create_assymetric_mapping(uuid_1, uuid_2)
+            if success & symmetric:
+                success = self._create_assymetric_mapping(uuid_2, uuid_1)
+            # Todo: What to do if first mapping succeeds but the other fails?
+        return (success, error_msg)
+
 
 def main():
     lora_hostname = 'http://mox-steffen:8080'
     mapper = KlasseMapper(lora_hostname)
 
-    uuid_1 = 'd8043094-6f38-4349-9fbb-dc7c28668fa0'
-    uuid_2 = '08857eb8-a2c4-4337-836f-19332f991362'
-    #print(mapper._create_assymetric_mapping(uuid_1, uuid_2))
+    uuid_1 = 'ee8dd627-9ff1-47c2-b900-aa3c214a31ee'
+    uuid_2 = 'a88aa93b-8edc-46ab-bad7-6535f9b765e5'
+
+    print(mapper.create_mapping(uuid_1, uuid_2))
+
     klasse_list = mapper.read_klasse_list()
     for uuid in klasse_list:
         mapping = mapper.read_mappings(uuid)
-        if mapping:
+        if not mapping:
             print(uuid)
             print(mapper.basic_klasse_info(uuid))
             print('-----------')
+
 
 if __name__ == '__main__':
     main()
