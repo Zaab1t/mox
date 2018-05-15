f2c16b151a1732c2e9f0cadec584d002f9f4db67
Robert Jensen <jensen.robert@gmail.com>
2018-05-15 14:26:32 +0200

    Significant performance optimization
    The read operations where very slow on a large data set.
    This commit improves performance. Further improvements are still
    wanted, total read of KLE still takes ~4s which is quite slow for
    a gui.

--- apps/lora_helpers.py
+++ apps/lora_helpers.py
@@ -1,6 +1,8 @@
 # encoding: utf-8
 import requests
+import grequests
 import settings
+
 
 class LoraHelper(object):
     """ Small set of function to help with LoRa maintenance """
@@ -11,7 +13,6 @@ class LoraHelper(object):
         :param hostname: hostname for the rest interface
         """
         self.hostname = hostname
-        self.request = requests.Session()
 
     def _read_type_list(self, url):
         """ Get a list of a uuids of a specific type
@@ -22,7 +23,7 @@ class LoraHelper(object):
         response = requests.get(self.hostname + url, params=args)
         klasse_list = response.json()['results'][0]
         return klasse_list
-        
+
     def read_klasse_list(self):
         """ Get a list of all avaiable Klasser
         :return: List of uuid's of all Klasser
@@ -30,29 +31,54 @@ class LoraHelper(object):
         url = '/klassifikation/klasse/'
         return self._read_type_list(url)
 
-    def read_klasse(self, uuid):
+    def read_klasser(self, uuids):
         """
-        Read a Klasse from LoRa
-        :param resource:    Name of the resource path/entity
-        :return:            Returns a dict with the Klasse
+        Read a list of Klasser from LoRa
+        :param uuids:  Name of the resource path/entity
+        :return: Returns a list of dicts with the complete Klasse
         """
-        url = '/klassifikation/klasse/'
-        #response = requests.get(self.hostname + url + uuid)
-        response = self.request.get(self.hostname + url + uuid)
-        klasse = response.json()[uuid][0]['registreringer'][0]
-        return klasse
+        klasse_url = self.hostname + '/klassifikation/klasse?uuid='
+        complete_urls = []
+        i = 0
+        param = ''
+        for _ in range(0, len(uuids)):
+            param = param + uuids[i] + '&uuid='
+            if i > 94:  # Max length of a request to LoRa
+                complete_urls.append(klasse_url + param[:-6])
+                i = 0
+                param = ''
+            else:
+                i += 1
+        complete_urls.append(klasse_url + param[:-6])
 
-    def basic_klasse_info(self, klasse):
-        """ Read human-readable data from Klass
-        :param klasse: Klasse object as return by read_klasse, or uuid
+        unsent_request = (grequests.get(url) for url in complete_urls)
+        results = grequests.map(unsent_request, size=5)
+        klasse_list = []
+        klasse_samlinger = [result.json() for result in results]
+        for klasse_samling in klasse_samlinger:
+            klasse_samling = klasse_samling['results'][0]
+            for klasse in klasse_samling:
+                uuid = klasse['id']
+                klasse = klasse['registreringer'][0]
+                klasse['uuid'] = uuid
+                klasse_list.append(klasse)
+        return klasse_list
+
+    def basic_klasse_info(self, klasser):
+        """ Read human-readable data from Klasse
+        :param klasse: List of Klasse object as return by read_klasse, or uuids
         :return: Information about the Klasse ie. Brugervendtnoegle
         """
-        if isinstance(klasse, unicode):
-            klasse = self.read_klasse(klasse)
-        egenskaber = klasse['attributter']['klasseegenskaber'][0]
-        info = egenskaber['brugervendtnoegle']
-        return info   
-    
+        #  If first element is a uuid, all elements are uuids...
+        if not isinstance(klasser[0], dict):
+            klasser = self.read_klasser(klasser)
+
+        info = []
+        for klasse in klasser:
+            egenskaber = klasse['attributter']['klasseegenskaber'][0]
+            info.append(egenskaber['brugervendtnoegle'])
+        return info
+
     def read_facet_list(self):
         """ Get a list of all avaiable Facetter
         :return: List of uuid's of all Facetter
@@ -78,15 +104,15 @@ class LoraHelper(object):
             return_val = False
         return return_val
 
+
 def main():
     helper = LoraHelper(settings.host)
-    #print(helper.delete_facet('81b80fa7-b71b-4d33-b528-cae03820875'))
-    #print(helper.read_facet_list())
-    #print(helper.delete_all_facetter())
+    # print(helper.delete_facet('81b80fa7-b71b-4d33-b528-cae03820875'))
+    # print(helper.read_facet_list())
+    # print(helper.delete_all_facetter())
     klasse_list = helper.read_klasse_list()
-    for klasse in klasse_list[0:100]:
-        print(helper.basic_klasse_info(klasse))
-
+    all_klasser = helper.read_klasser(klasse_list)
+    print(helper.basic_klasse_info(all_klasser))
 
 if __name__ == '__main__':
     main()
